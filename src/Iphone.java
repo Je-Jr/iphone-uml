e8e2b81fc08d179e340f34dab71c6d8a9b483660
Jorge Junior <jr.jorge500@yahoo.com>
2024-06-14 16:40:27 -0300

    Criando repositorio

---
+++ src/Iphone.java
@@ -0,0 +1,48 @@
+public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+
+    @Override
+    public void exibirPagina(String url) {
+        // Exibe pagina a partir de link
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        // Adiciona uma nova aba
+    }
+
+    @Override
+    public void atualizarPagina() {
+        // Atualiza pagina atual
+    }
+
+    @Override
+    public void ligar(String numero) {
+        // Liga para um número
+    }
+
+    @Override
+    public void atender() {
+        // Atende uma ligação
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        // Inicia correio de voz
+    }
+
+    @Override
+    public void tocar() {
+        // Toca uma música
+    }
+
+    @Override
+    public void pausar() {
+        // Pausa música atual
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        // Seleciona uma música
+    }
+    
+}
